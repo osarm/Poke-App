3b451ef671d6acf801df74efb222e153acfefcd6
osarm <161492930+osarm@users.noreply.github.com>
2024-04-05 19:28:15 -0400

    Update scripts.js
    If applied, this commit will add an API to my pokedex to call upon info from the PokemonAPI website

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,22 @@
-// height in meters(m)
-let pokemonRepository = (function(){
+let pokemonRepository = (function () {
+let pokemonList = [];
+let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-let pokemonList = [
-    {name: 'Bulbasaur', type: ['Grass', 'Poison'], height: 0.7},
-    {name: 'Ivysaur', type: ['Grass', 'Poison'], height: 1},
-    {name: 'Venusaur', type: ['Grass', 'Poison'], height: 2},
-    {name: 'Charmander', type: ['Fire'], height: 0.6},
-    {name: 'Charmeleon', type: ['Fire'], height: 1.1},
-    {name: 'Charizard', type: ['Fire', 'Flying'], height: 1.7},
-    {name: 'Squirtle', type: ['Water'], height: 0.5},
-    {name: 'Wartortle', type: ['Water'], height: 1},
-    {name: 'Blastoise', type: ['Water'], height: 1.6},
-    {name: 'Oshawott', type: ['Water'], height: 0.5}
-    ];
 function add(pokemon){
-    if (typeof pokemon === 'object') {
-        pokemonList.push(pokemon);
-    }
+ if (
+    typeof pokemon === "object" &&
+    "name" in pokemon
+  ) {
+    pokemonList.push(pokemon);
+  } else {
+    console.log("pokemon is not correct");
+  }
 }
-function getAll(){
+
+function getAll() {
     return pokemonList;
 }
+
 function addListItem(pokemon){
     let pokemonList= document.querySelector('.pokemon-list');
     let listpokemon = document.createElement('li');
@@ -30,33 +26,60 @@ function addListItem(pokemon){
     button.classList.add('button-class')
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
+    button.addEventListener("click", function(event) {
+        showDetails(pokemon);
+    });
     }
 
-function showDetails(pokemon){
-    console.log(pokemon);
-    }
+function loadList() {
+    return fetch(apiUrl).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        json.results.forEach(function (item) {
+            let pokemon = {
+                name: item.name,
+                detailsUrl: item.url
+            };
+            add(pokemon);
+            console.log(pokemon);
+        });
+    }).catch(function (e) {
+        console.error(e);
+    })
+}    
 
-function getPokemonByName(searchedName){
-    return pokemonList.filter((pokemon) => pokemon.name.toLowerCase() === searchedName.toLowerCase());
-}
+function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+ function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
     // returns the functions
-return{
+  return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
-    getPokemonByName: getPokemonByName,
+    loadList: loadList,
+    loadDetails: loadDetails,
     showDetails: showDetails
-    }
-})()
+  };
+})();
 
-pokemonRepository.add({
-    name: 'Dewott', height: 0.8, type: ['Water']
-});
-
-// prints the list of names from Pokemon List and their height
-pokemonRepository.getAll().forEach(function(pokemon){
-    // condition for pokemon with a height greater than 1m
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
+  });
 });
-
-console.log(pokemonRepository.getPokemonByName('oshawott'));
